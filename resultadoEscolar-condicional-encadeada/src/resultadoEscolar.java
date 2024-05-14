b4fd0ff5b5762990ea4917d3cf4f3f02c8c631c8
JOSEFRAN1980 <josefran.oliveira@gmail.com>
2024-05-14 05:00:48 -0300

    RESULTADO ESCOLAR- CONDICIONAL ENCADEADA

---
+++ resultadoEscolar-condicional-encadeada/src/resultadoEscolar.java
@@ -0,0 +1,18 @@
+public class resultadoEscolar {
+    public static void main(String[] args) throws Exception {
+         int nota = 6;
+
+         if (nota >= 7)
+         System.out.println("APROVADO");
+         
+         else if (nota >= 5 && nota < 7)
+         System.out.println("RECUPERAÇÃO");
+            
+         else
+         System.out.println("REPROVADO");
+            
+         
+
+
+    }
+}
